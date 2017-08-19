61cfef744251d90f88607cbe5b6b93ca7614019f
Rob Rix <rob.rix@me.com>
2017-08-19 09:24:58 -0400

    Pass DynFlags to styleUnqualified.

--- core/GhcMod/Doc.hs
+++ core/GhcMod/Doc.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE CPP #-}
 module GhcMod.Doc where
 
+import DynFlags
 import GHC
 import GhcMod.Gap (withStyle, showDocWith)
 import Outputable
@@ -14,7 +16,16 @@ showOneLine dflag style = showDocWith dflag OneLineMode . withStyle dflag style
 getStyle :: GhcMonad m => m PprStyle
 getStyle = do
     unqual <- getPrintUnqual
+#if __GLASGOW_HASKELL__ >= 802
+    dflags <- getDynFlags
+    return $ mkUserStyle dflags unqual AllTheWay
+#else
     return $ mkUserStyle unqual AllTheWay
+#endif
 
-styleUnqualified :: PprStyle
-styleUnqualified = mkUserStyle neverQualify AllTheWay
+styleUnqualified :: DynFlags -> PprStyle
+#if __GLASGOW_HASKELL__ >= 802
+styleUnqualified dflags = mkUserStyle dflags neverQualify AllTheWay
+#else
+styleUnqualified _ = mkUserStyle neverQualify AllTheWay
+#endif
